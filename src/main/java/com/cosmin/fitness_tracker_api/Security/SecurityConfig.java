4fd1ac4d90bbba7a45521f7acba6d94727957339
cosmiinn75 <anghelcosmin78@gmail.com>
2026-07-21 19:40:55 +0300

    Add actuator health endpoint

--- src/main/java/com/cosmin/fitness_tracker_api/Security/SecurityConfig.java
+++ src/main/java/com/cosmin/fitness_tracker_api/Security/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth ->
                         auth.requestMatchers("/api/auth/**").permitAll()
-                                .requestMatchers("/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**")
+                                .requestMatchers("/swagger-ui/**","/swagger-ui.html","/v3/api-docs/**","/actuator/health")
                                 .permitAll()
                                 .anyRequest().authenticated()
                 )
